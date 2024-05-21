dcea3def3b8249340237b2fa78ba2dd76f7aef76
07222002 <abbasaffandi2002@gamil.com>
2024-05-21 22:40:33 +0500

    dashboard page ui 11

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -7,7 +7,9 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ activeMenu }) => {
   return (
     <div className="flex fixed justify-between p-4 px-12 bg-white w-[100%]">
-      <h1 className="text-[30px] font-[800]">{activeMenu.charAt(0).toUpperCase() + activeMenu.slice(1)}</h1>
+      <h1 className="text-[30px] font-[800]">
+        {activeMenu.charAt(0).toUpperCase() + activeMenu.slice(1)}
+      </h1>
     </div>
   );
 };
